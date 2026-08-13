9ca8c806948eac86a51a0a20554212eb7e1c93c1
Ana <ana.c.santos611@aluno.senai.br>
2026-08-13 16:55:41 -0300

    salvo alterações

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { MenuComponentComponent } from './component/menu-component/menu-component.component';
+
+import { AtletaComponentComponent } from './component/atleta-component/atleta-component.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MenuComponentComponent, AtletaComponentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { AtletaComponentComponent } from './component/atleta-component/atleta-component.component';
+import { HomeComponentComponent } from './component/home-component/home-component.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+        path:"",
+        redirectTo:"/home",
+        pathMatch: "full"
+    },
+    {
+        path: "home",
+        component: HomeComponentComponent
+    },
+    {
+        path:"cadastroAtleta",
+        component: AtletaComponentComponent
+    }
+
+];
