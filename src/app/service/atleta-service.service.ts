e61385f278f7b174c26daa95a9ecbc32dbffaab0
Ana <ana.c.santos611@aluno.senai.br>
2026-08-14 16:20:42 -0300

    alterado o cadastro atleta

---
+++ src/app/service/atleta-service.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+import { Atleta } from '../models/atleta';  
+
+
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AtletaServiceService {
+    //DECLARANDO ARRAY
+    private atletas: Atleta[] = []
+
+    //DECLARAÇÃO DAS FUNÇÕES DE MANIPULAÇÃO DO ARRAY
+    //ADICIONADO ELEMENTO
+    adicionarAtleta(atleta: Atleta){
+      this.atletas.push(atleta)
+    }
+
+    //LISTAR ELEMENTOS
+    listarAtleta(){
+        console.table(this.atletas)
+
+        return this.atletas
+    }
+
+    //REMOVER ELEMENTO
+    removerElemento(idAtleta: number){
+        this.atletas = this.atletas.filter(elem=>elem.id !== idAtleta)
+    }
+
+    //REMOVER ELEMENTO2
+    removerElemento2(atleta: Atleta){
+      let posArray = this.atletas.findIndex(elem=>elem.id !== atleta.id)
+      this.atletas.splice(1, posArray)
+    }
+
+    //ALTERANDO ELEMENTO DO ARRAY
+    alterarElemento(atleta: Atleta){
+      let posArray = this.atletas.findIndex(elem=>elem.id !== atleta.id)
+      this.atletas[posArray] = atleta
+    }
+  constructor() { }
+}
